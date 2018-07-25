625e380034c629bd7bc348df4ba0ce3dc596436f
kate harris <kateharris03@gmail.com>
2018-07-25 15:12:59 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -19,13 +19,10 @@ change_rainbow_colors
 
 def add_colors
   rainbow_colors = [
-  "yellow",
-  "default",
-  "light_cyan"
+  "red",
+  "light_red",
+  "light_yellow"
 ]
-  rainbow_colors[0] = "red"
-  rainbow_colors[1] = "light_red"
-  rainbow_colors[2] = "light_yellow"
   rainbow_colors.push = "green"
   rainbow_colors.push = "blue"
   puts rainbow_colors
