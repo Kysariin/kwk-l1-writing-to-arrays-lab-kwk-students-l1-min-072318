12f13a30c47000166593ef62d66d0cbcd1fa5759
kate harris <kateharris03@gmail.com>
2018-07-25 15:10:04 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -8,6 +8,7 @@ def change_rainbow_colors
   puts rainbow_colors[0] = "red"
   puts rainbow_colors[1] = "light_red"
   puts rainbow_colors[2] = "light_yellow"
+  puts rainbow_colors
 end
 
 def add_colors
